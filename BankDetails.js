c19f4da2797e2058c77ba51ab7388545b8e0d868
sandhi agarwal <33195661+sandhi18@users.noreply.github.com>
2019-08-19 21:32:40 +0530

    Update BankDetails.js

--- BankDetails.js
+++ BankDetails.js
@@ -18,7 +18,7 @@ const pool = new Pool.Pool({
 
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 3020, () => console.log('Listening at port %d in %s mode", this.address().port));
+app.listen(process.env.PORT || 3020, () => console.log('Listening at port %d in %s mode', this.address().port));
 
 
 function isAuthenticated(req, res, next) {
